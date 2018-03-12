fc658d1e4e1665812796b283435c158263fcaa01
Ethandyp <duyupeng1@163.com>
2018-03-12 11:10:31 +0800

    centos7.1开机优化

---
+++ youhua.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#author:Ethan
+#date:20180312
+#version:1
+#centos:7
+
+#设置启动级别
+RUNLEVEL="multi-user"
+CRUNLEVEL=`systemctl get-default`
+echo "current run level: $CRUNLEVEL"
+if [[ ! $CRUNLEVEL =~ $RUNLEVEL ]]
+then
+    echo "changing to level: $RUNLEVEL"
+    systemctl set-default $RUNLEVEL
+    systemctl isolate $RUNLEVEL
+
+#更新yum源
+rm -rf /etc/yum.repos.d/*.repo
+#mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+yum makecache
+
